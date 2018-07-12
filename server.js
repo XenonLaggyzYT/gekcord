abc63543b1f5f5476cd3be705919f004f643e747
Glitch (gekcord) <none>
2018-07-12 21:03:00 +0000

    🏐🐢 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://gekcord.glitch.me/`);
+}, 280000);
+
+
